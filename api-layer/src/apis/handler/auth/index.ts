720f900dd516493cc6a80002e8b4eae8a1260aaa
vinay-satva-4221 <vinayp08042@gmail.com>
2023-06-30 14:37:01 +0530

    feat (handler) : add handler in structure

---
+++ api-layer/src/apis/handler/auth/index.ts
@@ -0,0 +1,38 @@
+import {commanResponse} from "../../utils/models/commonResponse";
+import * as Yup from "yup";
+import {ResponseStatus} from "../../utils/interfaces/enum";
+import {GET} from "../../utils/Requests";
+import {userAuthEndPoints} from "../../endpoints";
+export const authHandler ={
+    register
+}
+async function register (object: any) {
+    try {
+        let responseOBJ:commanResponse = new commanResponse();
+
+        //schema for payload
+        let registerSchema = Yup.object({
+            name: Yup.string().required().min(3).max(30),
+            email: Yup.string().email('Not a proper email').required(),
+            password: Yup.string().required('password is require').matches(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+            repeat_password:Yup.ref('password')
+        });
+
+        if (await registerSchema.isValid(object)) {
+            const response = await GET(userAuthEndPoints.REGISTER_USER);
+
+            if (response) {
+                responseOBJ.Status = response?.status === 200 ? true : false;
+                responseOBJ.Result = response?.data ? response?.data : undefined;
+                responseOBJ.ResponseStatus = response.status;
+            }
+        } else {
+            responseOBJ.ResponseStatus = ResponseStatus.UnprocessableEntity;
+            responseOBJ.Status = false;
+            responseOBJ.Message = await registerSchema.validate(object).catch((e) => e.message);
+        }
+        return responseOBJ;
+    } catch (error) {
+        console.log("error From controller::>", error);
+    }
+}
